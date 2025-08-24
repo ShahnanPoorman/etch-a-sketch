73257769ca2f71e97b4859af308245e6aecdb3cb
ShahnanPoorman <shahnanpoorman@gmail.com>
2025-08-24 17:51:08 -0400

    Add error checking for range of size (1-100)

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ const grid_stats = document.querySelector('#grid-size');
 
 window.addEventListener('load', makeGrid());
 
+const butt = document.querySelector('#new-grid');
+butt.addEventListener("click", () => {
+    let dimensions = prompt('New grid squares per side?(max 100) e.g. \'16\' ');
+    (dimensions) ? makeGrid(dimensions) : makeGrid();
+
+})
+
 function makeGrid(squares = 16){
-    container.innerHTML=''
+    container.innerHTML='';
+    (squares > 100) ? (squares = 100) : null;
+    (squares < 1) ? (squares = 16) : null;
+
     for (let i = 0; i < squares*squares; i++) {
         const newDiv = document.createElement("div");
 
@@ -20,11 +30,3 @@ function makeGrid(squares = 16){
     }
 
 }
-
-const butt = document.querySelector('#new-grid');
-butt.addEventListener("click", () => {
-    let dimensions = prompt('New grid squares per side? e.g. \'16\' ');
-    (dimensions) ? makeGrid(dimensions) : makeGrid();
-
-})
-
