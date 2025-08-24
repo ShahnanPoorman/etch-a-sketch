80520e9d82e0f77307f4c2532f3cdc80813e476f
ShahnanPoorman <shahnanpoorman@gmail.com>
2025-08-24 17:29:15 -0400

    Add button to resize grid

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ const container = document.querySelector('#container')
 
 for(let i = 0; i < 256; i++){
     const newDiv = document.createElement("div");
-    newDiv.style.height = '50px';
-    newDiv.style.width = '50px';
-    newDiv.style.display = 'inline-block';
+
+    newDiv.style.flex = '1 0 6.25%';
     newDiv.style.backgroundColor = '';
     newDiv.style.border = '.5px solid lightgrey';
 
@@ -15,4 +14,29 @@ for(let i = 0; i < 256; i++){
 
     container.appendChild(newDiv);
 }
+
+function makeGrid(squares = 16){
+    container.innerHTML=''
+    for (let i = 0; i < squares*squares; i++) {
+        const newDiv = document.createElement("div");
+
+        newDiv.style.flex = `1 0 ${100/squares}%`;
+        newDiv.style.backgroundColor = '';
+        newDiv.style.border = '.5px solid lightgrey';
+
+        newDiv.addEventListener('mouseover', (e) => {
+            e.target.classList.toggle('sketched');
+        })
+
+        container.appendChild(newDiv);
+    }
+
+}
+
+const butt = document.querySelector('#new-grid');
+butt.addEventListener("click", () => {
+    let dimensions = prompt('New grid squares per side? e.g. \'16\' ');
+    (dimensions) ? makeGrid(dimensions) : makeGrid();
+
+})
 
